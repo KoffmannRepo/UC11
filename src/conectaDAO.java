7d5cbd42a46dd861137868e451198d349e9b5b4c
KoffmannRepo <137730078+KoffmannRepo@users.noreply.github.com>
2023-07-02 16:01:56 -0300

    conectaDAO método para desconectar do bd

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -2,32 +2,39 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
-
-
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author Adm
  */
 public class conectaDAO {
-    
-    public Connection connectDB(){
-        Connection conn = null;
-        
+
+    Connection conn;
+
+    public Connection connectDB() {
+
         try {
-        
+
             conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=admin");
-            
-        } catch (SQLException erro){
+
+        } catch (SQLException erro) {
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
         }
         return conn;
     }
-    
+
+    public void disconnectDB() {
+        try {
+            conn.close();
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+        }
+    }
 }
